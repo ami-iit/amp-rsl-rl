bd2ebbd5706bf8ee2372de3c974dbaae9500e3d4
Giulio Romualdi <giulio.romualdi@gmail.com>
2025-03-28 12:24:03 +0100

    Add initial implementation of amp_rsl_rl package with core modules and classes

---
+++ amp_rsl_rl/algorithms/amp_ppo.py
@@ -0,0 +1,393 @@
+# Copyright (c) 2025, Istituto Italiano di Tecnologia
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from rsl_rl.modules import ActorCritic
+from rsl_rl.storage import RolloutStorage
+
+from amp_rsl_rl.storage import ReplayBuffer
+from amp_rsl_rl.networks import Discriminator
+
+from amp_rsl_rl.utils import AMPLoader
+
+
+class AMP_PPO:
+    actor_critic: ActorCritic
+
+    def __init__(
+        self,
+        actor_critic,
+        discriminator,
+        amp_data,
+        amp_normalizer,
+        num_learning_epochs=1,
+        num_mini_batches=1,
+        clip_param=0.2,
+        gamma=0.998,
+        lam=0.95,
+        value_loss_coef=1.0,
+        entropy_coef=0.0,
+        learning_rate=1e-3,
+        max_grad_norm=1.0,
+        use_clipped_value_loss=True,
+        schedule="fixed",
+        desired_kl=0.01,
+        amp_replay_buffer_size=100000,
+        device="cpu",
+    ):
+        self.device = device
+
+        self.desired_kl = desired_kl
+        self.schedule = schedule
+        self.learning_rate = learning_rate
+
+        # Discriminator components
+        self.discriminator: Discriminator = discriminator.to(self.device)
+        self.amp_transition = RolloutStorage.Transition()
+
+        # The AMP discriminator takes as input the concatenation of the observations. So the replay buffer dimension
+        # is half the dimension of the discriminator input.
+        obs_dim = self.discriminator.input_dim // 2
+        self.amp_storage = ReplayBuffer(obs_dim, amp_replay_buffer_size, device)
+        self.amp_data: AMPLoader = amp_data
+        self.amp_normalizer = amp_normalizer
+
+        # PPO components
+        self.actor_critic = actor_critic
+        self.actor_critic.to(self.device)
+        self.storage = None  # initialized later
+
+        # Optimizer for policy and discriminator.
+        # TODO check if the l_2 normalization weight must be different for the discriminator
+        params = [
+            {"params": self.actor_critic.parameters(), "name": "actor_critic"},
+            {
+                "params": self.discriminator.trunk.parameters(),
+                "weight_decay": 10e-4,
+                "name": "amp_trunk",
+            },
+            {
+                "params": self.discriminator.linear.parameters(),
+                "weight_decay": 10e-2,
+                "name": "amp_head",
+            },
+        ]
+        self.optimizer = optim.Adam(params, lr=learning_rate)
+        self.transition = RolloutStorage.Transition()
+
+        # PPO parameters
+        self.clip_param = clip_param
+        self.num_learning_epochs = num_learning_epochs
+        self.num_mini_batches = num_mini_batches
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+        self.gamma = gamma
+        self.lam = lam
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
+
+    def init_storage(
+        self,
+        num_envs,
+        num_transitions_per_env,
+        actor_obs_shape,
+        critic_obs_shape,
+        action_shape,
+    ):
+        self.storage = RolloutStorage(
+            num_envs,
+            num_transitions_per_env,
+            actor_obs_shape,
+            critic_obs_shape,
+            action_shape,
+            device=self.device,
+        )
+
+    def test_mode(self):
+        self.actor_critic.test()
+
+    def train_mode(self):
+        self.actor_critic.train()
+
+    def act(self, obs, critic_obs):
+        if self.actor_critic.is_recurrent:
+            self.transition.hidden_states = self.actor_critic.get_hidden_states()
+        # Compute the actions and values
+        self.transition.actions = self.actor_critic.act(obs).detach()
+        self.transition.values = self.actor_critic.evaluate(critic_obs).detach()
+        self.transition.actions_log_prob = self.actor_critic.get_actions_log_prob(
+            self.transition.actions
+        ).detach()
+        self.transition.action_mean = self.actor_critic.action_mean.detach()
+        self.transition.action_sigma = self.actor_critic.action_std.detach()
+        # need to record obs and critic_obs before env.step()
+        self.transition.observations = obs
+        self.transition.critic_observations = critic_obs
+        return self.transition.actions
+
+    def act_amp(self, amp_obs):
+        self.amp_transition.observations = amp_obs
+
+    def process_env_step(self, rewards, dones, infos):
+        self.transition.rewards = rewards.clone()
+        self.transition.dones = dones
+        # Bootstrapping on time outs
+        if "time_outs" in infos:
+            self.transition.rewards += self.gamma * torch.squeeze(
+                self.transition.values
+                * infos["time_outs"].unsqueeze(1).to(self.device),
+                1,
+            )
+
+        # Record the transition
+        self.storage.add_transitions(self.transition)
+        self.transition.clear()
+        self.actor_critic.reset(dones)
+
+    def process_amp_step(self, amp_obs):
+        self.amp_storage.insert(self.amp_transition.observations, amp_obs)
+        self.amp_transition.clear()
+
+    def compute_returns(self, last_critic_obs):
+        last_values = self.actor_critic.evaluate(last_critic_obs).detach()
+        self.storage.compute_returns(last_values, self.gamma, self.lam)
+
+    def discriminator_policy_loss(self, discriminator_output):
+        loss = torch.nn.BCEWithLogitsLoss()
+        expected = torch.zeros_like(discriminator_output).to(self.device)
+        return loss(discriminator_output, expected)
+
+    def discriminator_expert_loss(self, discriminator_output):
+        loss = torch.nn.BCEWithLogitsLoss()
+        expected = torch.ones_like(discriminator_output).to(self.device)
+        return loss(discriminator_output, expected)
+
+    def update(self):
+        mean_value_loss = 0
+        mean_surrogate_loss = 0
+        mean_amp_loss = 0
+        mean_grad_pen_loss = 0
+        mean_policy_pred = 0
+        mean_expert_pred = 0
+        mean_accuracy_policy = 0
+        mean_accuracy_expert = 0
+        mean_accuracy_policy_elem = 0
+        mean_accuracy_expert_elem = 0
+        if self.actor_critic.is_recurrent:
+            generator = self.storage.reccurent_mini_batch_generator(
+                self.num_mini_batches, self.num_learning_epochs
+            )
+        else:
+            generator = self.storage.mini_batch_generator(
+                self.num_mini_batches, self.num_learning_epochs
+            )
+
+        # Get the iterator for the data generated by the policy
+        amp_policy_generator = self.amp_storage.feed_forward_generator(
+            self.num_learning_epochs * self.num_mini_batches,
+            self.storage.num_envs
+            * self.storage.num_transitions_per_env
+            // self.num_mini_batches,
+        )
+
+        # Get the iterator for the data from the dataset
+        amp_expert_generator = self.amp_data.feed_forward_generator(
+            self.num_learning_epochs * self.num_mini_batches,
+            self.storage.num_envs
+            * self.storage.num_transitions_per_env
+            // self.num_mini_batches,
+        )
+
+        # Iterate over the data generated by the policy and the dataset in the following loop we train both the actor-crictic and the AMP discriminator
+        for sample, sample_amp_policy, sample_amp_expert in zip(
+            generator, amp_policy_generator, amp_expert_generator
+        ):
+
+            # scomposing the sample
+            (
+                obs_batch,
+                critic_obs_batch,
+                actions_batch,
+                target_values_batch,
+                advantages_batch,
+                returns_batch,
+                old_actions_log_prob_batch,
+                old_mu_batch,
+                old_sigma_batch,
+                hid_states_batch,
+                masks_batch,
+                rnd_state_batch,
+            ) = sample
+
+            self.actor_critic.act(
+                obs_batch, masks=masks_batch, hidden_states=hid_states_batch[0]
+            )
+            actions_log_prob_batch = self.actor_critic.get_actions_log_prob(
+                actions_batch
+            )
+            value_batch = self.actor_critic.evaluate(
+                critic_obs_batch, masks=masks_batch, hidden_states=hid_states_batch[1]
+            )
+            mu_batch = self.actor_critic.action_mean
+            sigma_batch = self.actor_critic.action_std
+            entropy_batch = self.actor_critic.entropy
+
+            # KL
+            if self.desired_kl is not None and self.schedule == "adaptive":
+                with torch.inference_mode():
+                    kl = torch.sum(
+                        torch.log(sigma_batch / old_sigma_batch + 1.0e-5)
+                        + (
+                            torch.square(old_sigma_batch)
+                            + torch.square(old_mu_batch - mu_batch)
+                        )
+                        / (2.0 * torch.square(sigma_batch))
+                        - 0.5,
+                        axis=-1,
+                    )
+                    kl_mean = torch.mean(kl)
+
+                    if kl_mean > self.desired_kl * 2.0:
+                        self.learning_rate = max(1e-5, self.learning_rate / 1.5)
+                    elif kl_mean < self.desired_kl / 2.0 and kl_mean > 0.0:
+                        self.learning_rate = min(1e-2, self.learning_rate * 1.5)
+
+                    for param_group in self.optimizer.param_groups:
+                        param_group["lr"] = self.learning_rate
+
+            # Surrogate loss
+            ratio = torch.exp(
+                actions_log_prob_batch - torch.squeeze(old_actions_log_prob_batch)
+            )
+            surrogate = -torch.squeeze(advantages_batch) * ratio
+            surrogate_clipped = -torch.squeeze(advantages_batch) * torch.clamp(
+                ratio, 1.0 - self.clip_param, 1.0 + self.clip_param
+            )
+            surrogate_loss = torch.max(surrogate, surrogate_clipped).mean()
+
+            # Value function loss
+            if self.use_clipped_value_loss:
+                value_clipped = target_values_batch + (
+                    value_batch - target_values_batch
+                ).clamp(-self.clip_param, self.clip_param)
+                value_losses = (value_batch - returns_batch).pow(2)
+                value_losses_clipped = (value_clipped - returns_batch).pow(2)
+                value_loss = torch.max(value_losses, value_losses_clipped).mean()
+            else:
+                value_loss = (returns_batch - value_batch).pow(2).mean()
+
+            ppo_loss = (
+                surrogate_loss
+                + self.value_loss_coef * value_loss
+                - self.entropy_coef * entropy_batch.mean()
+            )
+
+            # Compute the AMP loss
+            policy_state, policy_next_state = sample_amp_policy
+            expert_state, expert_next_state = sample_amp_expert
+            if self.amp_normalizer is not None:
+                with torch.no_grad():
+                    policy_state = self.amp_normalizer.normalize_torch(
+                        policy_state, self.device
+                    )
+                    policy_next_state = self.amp_normalizer.normalize_torch(
+                        policy_next_state, self.device
+                    )
+                    expert_state = self.amp_normalizer.normalize_torch(
+                        expert_state, self.device
+                    )
+                    expert_next_state = self.amp_normalizer.normalize_torch(
+                        expert_next_state, self.device
+                    )
+
+            # compute the discriminator output for the policy and expert data (the output is the logit of the discriminator)
+            policy_d = self.discriminator(
+                torch.cat([policy_state, policy_next_state], dim=-1)
+            )
+            expert_d = self.discriminator(
+                torch.cat([expert_state, expert_next_state], dim=-1)
+            )
+
+            # compute the loss for the discriminator
+            expert_loss = self.discriminator_expert_loss(expert_d)
+            policy_loss = self.discriminator_policy_loss(policy_d)
+
+            # compute the AMP loss
+            amp_loss = 0.5 * (expert_loss + policy_loss)
+
+            # As presented in the paper, the gradient penalty is used to stabilize the training of the discriminator
+            grad_pen_loss = self.discriminator.compute_grad_pen(
+                *sample_amp_expert, lambda_=10
+            )
+
+            # The final loss is the sum of the PPO loss, the AMP loss and the gradient penalty loss
+            loss = ppo_loss + (amp_loss + grad_pen_loss)
+
+            # Gradient step
+            self.optimizer.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+            self.optimizer.step()
+
+            # TODO is this required???
+            # if not self.actor_critic.fixed_std and self.min_std is not None:
+            #     self.actor_critic.std.data = self.actor_critic.std.data.clamp(min=self.min_std)
+
+            # Update the running mean and std of the normalizer
+            if self.amp_normalizer is not None:
+                self.amp_normalizer.update(policy_state.cpu().numpy())
+                self.amp_normalizer.update(expert_state.cpu().numpy())
+
+            # compute the probability of the discriminator output from the logit
+            policy_d_prob = torch.sigmoid(policy_d)
+            expert_d_prob = torch.sigmoid(expert_d)
+
+            mean_value_loss += value_loss.item()
+            mean_surrogate_loss += surrogate_loss.item()
+            mean_amp_loss += amp_loss.item()
+            mean_grad_pen_loss += grad_pen_loss.item()
+            mean_policy_pred += policy_d_prob.mean().item()
+            mean_expert_pred += expert_d_prob.mean().item()
+
+            # compute the accuracy of the discriminator if the output is below 0.5 the discriminator is fooled by the policy
+            # if the output is above 0.5 the discriminator is fooled by the expert
+            mean_accuracy_policy += torch.sum(
+                torch.round(policy_d_prob) == torch.zeros_like(policy_d_prob)
+            ).item()
+            mean_accuracy_expert += torch.sum(
+                torch.round(expert_d_prob) == torch.ones_like(expert_d_prob)
+            ).item()
+
+            # save the number of elements in the batch
+            mean_accuracy_expert_elem += expert_d_prob.numel()
+            mean_accuracy_policy_elem += policy_d_prob.numel()
+
+        num_updates = self.num_learning_epochs * self.num_mini_batches
+        mean_value_loss /= num_updates
+        mean_surrogate_loss /= num_updates
+        mean_amp_loss /= num_updates
+        mean_grad_pen_loss /= num_updates
+        mean_policy_pred /= num_updates
+        mean_expert_pred /= num_updates
+        mean_accuracy_policy /= mean_accuracy_policy_elem
+        mean_accuracy_expert /= mean_accuracy_expert_elem
+
+        self.storage.clear()
+
+        return (
+            mean_value_loss,
+            mean_surrogate_loss,
+            mean_amp_loss,
+            mean_grad_pen_loss,
+            mean_policy_pred,
+            mean_expert_pred,
+            mean_accuracy_policy,
+            mean_accuracy_expert,
+        )
